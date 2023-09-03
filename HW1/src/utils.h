3119acb2a86b88ffbe0714d2828705c39a786238
Samuel Alvinge <alvinge@student.chalmers.se>
2023-09-03 13:18:40 +0200

    renamed folders and added P2

--- HW1/src/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<string>
-#include<vector>
-#include "neuron.h"
-
-void print_state(size_t num_neurons, size_t num_columns, std::vector<int> states);
-std::string convert_for_printing(int state);
-std::vector<std::vector<int>> parse_patterns(const std::string& filename);
-std::string convert_state_to_scheme(const std::vector<int>& state);
