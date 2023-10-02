e07d398063b9ce3c93e292dfafd64b0f8aa9202f
Samuel Alvinge <alvinge@student.chalmers.se>
2023-10-02 10:06:23 +0200

    cleaning up before handing in

--- P3/src/main.cpp
+++ P3/src/main.cpp
@@ -7,7 +7,6 @@
 
 // main function for P2
 int main() {
-
     Data training_data = read_csv("data/training_set.csv");
     Data validation_data = read_csv("data/validation_set.csv");
 
@@ -20,27 +19,12 @@ int main() {
     double_vector learning_rates = {0.005, 0.01};
     double_vector momentums = {0.7};
     int_vector batch_sizes = {2};
-    bool grid_search = false;
 
-    if (grid_search) {
-        for (const auto &nr_neuron : nr_neurons) {
-            for (const auto &learning_rate : learning_rates) {
-                for (const auto &momentum : momentums) {
-                    for (const auto &batch_size : batch_sizes) {
-                        Network network(nr_neuron, training_data, validation_data);
-                        network.train(learning_rate, 0.0001, 1, momentum, batch_size, 350, true, false);
-                        std::cout << nr_neuron << " " << learning_rate << " " << momentum << " " << batch_size << std::endl;
-                    }
-                }
-            }
-        }
-    } else {
-        Network network(32, training_data, validation_data);
-        network.train(0.005, 0.0001, 1, 0.7, 2, 10000, true, true);
-        write_weights_and_biases_to_csv(network.get_weights_hl(), network.get_biases_hl(), network.get_weights_ol(), network.get_biases_ol());      
-        network.export_validation_results();
-        std::system("python plot.py");
-    }
+    Network network(32, training_data, validation_data);
+    network.train(0.005, 0.0001, 1, 0.7, 2, 10000, true, true);
+    write_weights_and_biases_to_csv(network.get_weights_hl(), network.get_biases_hl(), network.get_weights_ol(), network.get_biases_ol());      
+    network.export_validation_results();
+    std::system("python plot.py");
 
     return 0;
 };
