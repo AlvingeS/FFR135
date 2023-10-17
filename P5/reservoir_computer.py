f5c23ed41a13b74b1846ecd9346f36f0c5e97e74
Samuel Alvinge <alvinge@student.chalmers.se>
2023-10-17 17:06:47 +0200

    almost there

---
+++ P5/reservoir_computer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class ReservoirComputer:
+    def __init__(self, n_inputs, n_outputs, n_res_neurons, mean=0, input_variance=0.002, res_variance=2/500):
+        self.n_reservoir = n_res_neurons
+        self.input_to_res_weights = np.random.normal(mean, np.sqrt(input_variance), (n_res_neurons, n_inputs))
+        self.res_to_res_weights = np.random.normal(mean, np.sqrt(res_variance), (n_res_neurons, n_res_neurons))
+        self.res_to_output_weights = np.random.normal(mean, np.sqrt(res_variance), (n_outputs, n_res_neurons))
+        self.reservoir_state = np.zeros((n_res_neurons, 1))
+        self.output_state = np.zeros((n_outputs, 1))
+        
+    def propagate(self, input_signals):
+        # Update reservoir state
+        self.reservoir_state = np.tanh(np.dot(self.res_to_res_weights, self.reservoir_state) + np.dot(self.input_to_res_weights, input_signals))
+        self.output_state = np.dot(self.res_to_output_weights, self.reservoir_state)
